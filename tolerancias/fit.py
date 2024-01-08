495648b065bab488b1ba95c0be96ec3e239e587c
flyingbot91 <flyingbot91@gmx.com>
2024-01-09 00:01:38 +0100

    Add 'tolerancias.fit.Fit.__str__' method

--- tolerancias/fit.py
+++ tolerancias/fit.py
@@ -8,13 +8,26 @@ from .errors import FitError
 TOLERANCE_INDEXES = '|'.join(TOLERANCE_GRADES)
 HOLE_POSITIONS = 'A|B|C|CD|D|E|EF|F|FG|G|H|J|Js|K|M|N|P|R|S|T|U|V|X|Y|Z|ZA|ZB|ZC'
 SHAFT_POSITIONS = 'a|b|c|cd|d|e|ef|f|fg|g|h|j|js|k|m|n|p|r|s|t|u|v|x|y|z|z|za|zb|zc'
+LALIGN = 20
+RALIGN = 5
 
 
 class Fit:
 
     def __init__(self, value: str) -> None:
         self.value = value
+        self.values = {}
         self._validate()
+
+    def __str__(self) -> str:
+        return f"{self.value}\n" \
+            f"{'Diameter:':<{LALIGN}} {self.values['diameter']:>{RALIGN}}\n" \
+            f"{'Hole:':<{LALIGN}}\n" \
+            f"{'  Position:':<{LALIGN}} {self.values['hole_position']:>{RALIGN}}\n" \
+            f"{'  Tolerance:':<{LALIGN}} {self.values['hole_tolerance']:>{RALIGN}}\n" \
+            f"{'Shaft:':<{LALIGN}}\n" \
+            f"{'  Position:':<{LALIGN}} {self.values['shaft_position']:>{RALIGN}}\n" \
+            f"{'  Tolerance:':<{LALIGN}} {self.values['shaft_tolerance']:>{RALIGN}}"
 
     def _validate(self) -> None:
         regex = r'(?P<diameter>\d+)' \
@@ -25,6 +38,6 @@ class Fit:
         pattern = re.compile(regex)
         data = pattern.match(self.value)
         try:
-            values = data.groupdict()
+            self.values = data.groupdict()
         except AttributeError:
             raise FitError(f"Fit value '{self.value}' does not match regex '{regex}'")
